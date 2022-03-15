c36165f490cf85c9269be3c127175b1042ff5150
abrahamomolewa <abrahamomolewacrib@gmail.com>
2022-03-15 20:38:22 +0100

    done

---
+++ src/data.js
@@ -0,0 +1,98 @@
+module.exports = {
+    "socials": [
+        {
+            "icon": "twitter",
+            "url": "https://twitter.com/omolewaabraham"
+        },
+        {
+            "icon": "github-circled",
+            "url": "https://github.com/abrahamomolewa"
+        },
+        {
+            "icon": "dribbble",
+            "url": "http://dribbble"
+        },
+        {
+            "icon": "skype",
+            "url": "htttp://skype"
+        },
+        {
+            "icon": "mail-alt",
+            "url": "mailto: email"
+        },
+        {
+            "icon": "linkedin-squared",
+            "url": "http://linkedin.com/in/abraham-omolewa"
+        }
+    ],
+    "experiences": [
+        {
+            "__key": "d6e2bc4d-e2a8-4b5a-9d15-e093ee060083",
+            "title": "Cybersecurity Intern at Telenoetica Limited",
+            "timeline": " 2019 ",
+            "description": ""
+        },
+        {
+            "__key": "c0648399-71be-4612-b366-1ed8f9e8607a",
+            "title": "Fullstack Software Engineer at Company B",
+            "timeline": "Feb 2015 - Dec 2015",
+            "responsibilities": [
+                "Worked with a global team of developers and artists for the implementation of new features in Existing Codebase of a Free-to-play MMO",
+                "Increased rate of tickets/tasks done by the team up to 300% within first few weeks!",
+                "Added Abstractions on Server API to Minimize Duplicate Code"
+            ],
+            "description": ""
+        }
+    ],
+    "name": "Abraham Omolewa",
+    "header": "Hi, I'm Omolewa",
+    "cta": {
+        "label": "get my resume",
+        "url": "resume.pdf"
+    },
+    "testimonials": [
+        {
+            "__key": "6b91ad1c-7081-425a-a3bb-007a52d7e3fe",
+            "quote": "Incredibly talented and hardworking. A super friendly guy who is a frequent communicator",
+            "name": "Jaime Noguera",
+            "title": "Project Manager"
+        },
+        {
+            "__key": "89116bbe-4cb5-4337-819d-49db972e0cfb",
+            "quote": "Wow, thank you for this, you probably have the most intuitive explanation for this problem!",
+            "name": "Xburr",
+            "title": "Client"
+        }
+    ],
+    "title": "Abraham Omolewa",
+    "projects": [
+        {
+            "__key": "cf57cd59-54ad-49d6-9bfc-02a68f5c97ca",
+            "name": "MintNFTui",
+            "description": "An NFT mint page that can be using in launching an NFT project",
+            "tags": [
+                "Typescript",
+                "CSS",
+                "HTML",
+                "Shell"
+            ],
+            "alt": "Combustion Screenshot",
+            "img": "combustion.png",
+            "url": "https://heuristic-hermann-438040.netlify.app/"
+        },
+        {
+            "__key": "71856514-6d8d-4fd3-ace7-6942af0d804a",
+            "name": "InvoiceApp",
+            "description": "An Invoice generating app. ",
+            "tags": [
+                "react",
+                "Bootstrap"
+            ],
+            "img": "merc-01.png",
+            "url": "https://basicinvoice.netlify.app/"
+        }
+    ],
+    "email": "abraham.omolewa@gmail.com",
+    "description": "I'm a result-oriented Software engineer with ability to learn and collaborate in\nrapidly changing environments and compositions. Worked\nthrough 1000+ hours of bootcamp structure, learning Ruby on rails, Style/Markup Languages, rust and Blockchain development. Eager to tackle software\ndevelopment/design challenges to achieve lasting impacts on\nuser experience",
+    "footerTitle": "Software Engineer"
+}
